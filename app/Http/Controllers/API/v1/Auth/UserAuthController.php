96324611acdb3c7b01d9faaa135f6b36f1a2965d
shroukelzoghby <shrouk.elzoghby1@gmail.com>
2025-02-10 06:55:42 +0200

    FEAT: Add logout functionality with token revocation

--- app/Http/Controllers/API/v1/Auth/UserAuthController.php
+++ app/Http/Controllers/API/v1/Auth/UserAuthController.php
@@ -49,6 +49,19 @@ class UserAuthController extends Controller
     }
     public function logout(Request $request)
     {
+        try{
+            $request->user()->tokens()->delete();
+            return successResponse(
+                message: 'Logout successfully',
+                statusCode: Response::HTTP_OK
+            );
+        }catch (Exception $e){
+            Log::error('Logout failed: '.$e->getMessage().' in '.$e->getFile().' on line '.$e->getLine().' with code '.$e->getCode());
+            return errorResponse(
+                message: 'Logout failed',
+                statusCode: Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
 
 
     }
