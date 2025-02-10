8f57647af224c58a9309f7dc8466c11805bb3e85
shroukelzoghby <shrouk.elzoghby1@gmail.com>
2025-02-10 19:58:39 +0200

    FEAT: Add validation rules to CheckOTPRequest

---
+++ app/Http/Requests/API/v1/CheckOTPRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\API\v1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CheckOTPRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email|exists:users,email',
+            'otp' => 'required|numeric|digits:5',
+        ];
+    }
+}
