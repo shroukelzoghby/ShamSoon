6cb3eb0e53203c25ad9855e9fccdfa0086ba43fb
shroukelzoghby <shrouk.elzoghby1@gmail.com>
2025-02-10 07:03:44 +0200

    FEAT: Add Endpoint for Logout

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,16 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('users')
     ->name('users.')
     ->group(function (){
+        // Public routes - No Need for authentication
         Route::post('/register',UserRegisterController::class)
             ->name('register');
         Route::post('/login',[UserAuthController::class,'authenticate'])
             ->name('login');
+
+        // Private routes - Need authentication
+        Route::middleware('auth:sanctum')
+            ->group(function () {
+                Route::post('/logout',[UserAuthController::class,'logout'])->name('logout');
+            });
 });
+
