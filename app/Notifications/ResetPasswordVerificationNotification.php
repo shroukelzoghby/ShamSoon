a2bcc743f36e4877f1592d73fc1636b36608f906
shroukelzoghby <shrouk.elzoghby1@gmail.com>
2025-02-20 01:07:08 +0200

    FEAT: Add Reset Password Verification Notification

---
+++ app/Notifications/ResetPasswordVerificationNotification.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Notifications;
+
+use Ichtrojan\Otp\Otp;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+
+class ResetPasswordVerificationNotification extends Notification
+{
+    use Queueable;
+    public $message;
+    public $subject;
+    public $fromEmail;
+    public $mailer;
+    private $otp;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+        $this->message="Use the below code for resetting your password";
+        $this->subject="Resetting your password";
+        $this->fromEmail="test@shroukelzoghby.com";
+        $this->mailer='smtp';
+        $this->otp = new Otp;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        $otp=$this->otp->generate($notifiable->email,'numeric',5,60);
+        return (new MailMessage)
+            ->mailer($this->mailer)
+            ->subject($this->subject)
+            ->greeting('Hello '.$notifiable->username)
+            ->line($this->message)
+            ->line('Code '.$otp->token);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
