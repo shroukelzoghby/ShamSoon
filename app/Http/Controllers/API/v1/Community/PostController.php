c6a02b4198a7c8e55c964188e1ca11e89cbe4530
shroukelzoghby <shrouk.elzoghby1@gmail.com>
2025-02-22 19:38:11 +0200

    FEAT: Add store post functionality with error handling

--- app/Http/Controllers/API/v1/Community/PostController.php
+++ app/Http/Controllers/API/v1/Community/PostController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\API\v1\Community;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\API\v1\PostRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class PostController extends Controller
 {
@@ -18,9 +21,21 @@ class PostController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(PostRequest $request)
     {
-        //
+        try {
+            $post = Auth::user()->posts()->create($request->validated());
+            return successResponse(
+                data: ['post' => $post],
+                message: 'Post created successfully',
+                statusCode: Response::HTTP_CREATED
+            );
+        } catch (\Exception $e) {
+            return errorResponse(
+                message: 'An error occurred while creating the post.',
+                statusCode: Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
     }
 
     /**
