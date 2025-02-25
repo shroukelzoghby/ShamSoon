d572867262af69a642c87fda47db6fb0e0b54d5b
ahmed <ahmedmohamed2042003@gmail.com>
2025-02-25 05:48:43 +0200

    FEAT: Add retrive all comments functionality with error handling

--- app/Http/Controllers/API/v1/Community/CommentController.php
+++ app/Http/Controllers/API/v1/Community/CommentController.php
@@ -2,17 +2,32 @@
 
 namespace App\Http\Controllers\API\v1\Community;
 
-use App\Http\Controllers\Controller;
+use App\Models\Post;
+use App\Models\Comment;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class CommentController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Post $post)
     {
-        //
+        try {
+            $comments = $post->comments()->with('user')->latest()->get();
+            return successResponse(
+                data: ['Comments' => $comments],
+                message: 'Comments retrieved successfully', 
+                statusCode: Response::HTTP_OK
+            );
+        } catch (\Exception $e) {
+            return errorResponse(
+                message: 'An error occurred while fetching Comments.',
+                statusCode: Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
     }
 
     /**
