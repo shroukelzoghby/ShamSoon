44b5a6ae0f7870e413b6ffdcac6b9ec16a27de5d
ahmed <ahmedmohamed2042003@gmail.com>
2025-02-25 06:12:35 +0200

    FEAT: Add store comment functionality with error handling

--- app/Http/Controllers/API/v1/Community/CommentController.php
+++ app/Http/Controllers/API/v1/Community/CommentController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\API\v1\Community;
 
+use App\Http\Requests\API\v1\CommentRequest;
 use App\Models\Post;
 use App\Models\Comment;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class CommentController extends Controller
@@ -33,9 +35,23 @@ class CommentController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CommentRequest $request,Post $post)
     {
-        //
+        try {
+            $validatedData = $request->validated(); 
+            $validatedData['user_id'] = Auth::id();
+            $comment = $post->comments()->create($validatedData);
+            return successResponse(
+                data: ['comment' => $comment],
+                message: 'Comment created successfully',
+                statusCode: Response::HTTP_CREATED
+            );
+        } catch (\Exception $e) {
+            return errorResponse(
+                message: 'An error occurred while creating the post.',
+                statusCode: Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
     }
 
     /**
