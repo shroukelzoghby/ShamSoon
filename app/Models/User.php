99411ec836d4e9ee9e250f28225b46d3074514bd
shroukelzoghby <shrouk.elzoghby1@gmail.com>
2025-03-10 05:55:48 +0200

    FEAT: Add Relation between users and roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -24,7 +25,8 @@ class User extends Authenticatable
         'phone',
         'password',
         'email_verified_at',
-        'social_id'
+        'social_id',
+        'role_id'
     ];
 
     /**
@@ -65,5 +67,10 @@ class User extends Authenticatable
     public function solarPanels()
     {
         return $this->hasMany(SolarPanel::class);
+    }
+
+    public function role(): BelongsTo
+    {
+        return $this->belongsTo(Role::class);
     }
 }
