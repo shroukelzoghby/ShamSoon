6600022e4b93ec886fac81345e2b7975f9e7faea
shroukelzoghby <shrouk.elzoghby1@gmail.com>
2025-03-18 04:27:11 +0200

    FEAT: Add SendNotification and Update Store in comment controller to notify user when someone comment on his post

--- app/Services/FirebaseNotificationService.php
+++ app/Services/FirebaseNotificationService.php
@@ -35,4 +35,18 @@ class FirebaseNotificationService
             throw new Exception("Failed to send notification: " . $e->getMessage());
         }
     }
+
+    public function sendNotification($token, $title, $body, $data = [])
+    {
+        try {
+            $message = CloudMessage::withTarget('token', $token)
+                ->withNotification(Notification::create($title, $body))
+                ->withData($data);
+
+            $this->messaging->send($message);
+            return true;
+        } catch (Exception $e) {
+            throw new Exception("Failed to send notification: " . $e->getMessage());
+        }
+    }
 }
