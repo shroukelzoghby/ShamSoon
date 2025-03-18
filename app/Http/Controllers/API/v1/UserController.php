b2c5a69b45d3c1ed19a55095fba6b332ae5eca9e
shroukelzoghby <shrouk.elzoghby1@gmail.com>
2025-03-18 02:33:26 +0200

    FEAT: Add logic to store FCM token in  function

--- app/Http/Controllers/API/v1/UserController.php
+++ app/Http/Controllers/API/v1/UserController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers\API\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\API\v1\StoreFCMTokenRequest;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
-    public function storeFcmToken(Request $request)
+    public function storeFcmToken(StoreFCMTokenRequest $request)
     {
+        $user = $request->user();
+
+        $user->fcm_token = $request->fcm_token;
+        $user->save();
+        return successResponse(
+            data: ['user' => $user],
+            message: 'FCM token stored successfully',
+            statusCode: Response::HTTP_OK
+        );
 
     }
 
