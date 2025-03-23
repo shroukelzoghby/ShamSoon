0d1ccb1e85409436c058c4c8738696ad5a582976
ahmed <ahmedmohamed2042003@gmail.com>
2025-03-23 18:57:51 +0200

    FIX: Return 404 when role is not found instead of 500

--- app/Http/Controllers/API/v1/Auth/UserRegisterController.php
+++ app/Http/Controllers/API/v1/Auth/UserRegisterController.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Controllers\API\v1\Auth;
 
-use App\Http\Resources\API\v1\UserResource;
+use Exception;
 use App\Models\Role;
 use App\Models\User;
-use App\Http\Controllers\Controller;
-use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\API\v1\UserResource;
 use Symfony\Component\HttpFoundation\Response;
 use App\Http\Requests\API\v1\UserRegisterRequest;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class UserRegisterController extends Controller
 {
@@ -23,7 +23,7 @@ class UserRegisterController extends Controller
                 $roleId = Role::where('name', 'user')->value('id');
 
                 if (!$roleId) {
-                    throw new Exception('Role not found', Response::HTTP_NOT_FOUND);
+                    throw new ModelNotFoundException('Role not found');
                 }
 
                 // Create a new user
@@ -35,20 +35,25 @@ class UserRegisterController extends Controller
             $token = $user->createToken('authToken')->plainTextToken;
 
             return successResponse(
-                data:[
-                    'user' => new UserResource($user) ,
+                data: [
+                    'user' => new UserResource($user),
                     'token' => $token,
-                    ],
+                ],
                 message: 'User created successfully',
                 statusCode: Response::HTTP_CREATED
             );
 
+        } catch (ModelNotFoundException $e) {
+            return errorResponse(
+                message: 'Role not found',
+                statusCode: Response::HTTP_NOT_FOUND
+            );
         } catch (\Exception $e) {
             logError('User registration failed', $e);
             return errorResponse(
                 message: 'Registration failed',
                 statusCode: Response::HTTP_INTERNAL_SERVER_ERROR,
-                errors:$e->getMessage(),
+                errors: $e->getMessage(),
             );
         }
 
