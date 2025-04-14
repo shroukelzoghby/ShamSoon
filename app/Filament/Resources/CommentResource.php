becb1b99b652e1aab1c20fec7366a2b4108a9ad5
ahmed <ahmedmohamed2042003@gmail.com>
2025-04-14 20:45:16 +0200

    FEAT: add navigationgroup and infolist to CommentResource

--- app/Filament/Resources/CommentResource.php
+++ app/Filament/Resources/CommentResource.php
@@ -2,33 +2,38 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\CommentResource\Pages;
-use App\Filament\Resources\CommentResource\RelationManagers;
-use App\Models\Comment;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
+use App\Models\Comment;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Filament\Infolists\Infolist;
+use Filament\Resources\Resource;
 use Illuminate\Database\Eloquent\Builder;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use App\Filament\Resources\CommentResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\CommentResource\RelationManagers;
 
 class CommentResource extends Resource
 {
     protected static ?string $model = Comment::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationGroup = 'System Community';
+    protected static ?int $navigationSort = 2;
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('user_id')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\TextInput::make('post_id')
-                    ->required()
-                    ->numeric(),
+                Forms\Components\select::make('user_id')
+                    ->relationship('user', 'name')
+                    ->required(),
+                Forms\Components\select::make('post_id')
+                    ->relationship('post', 'title')
+                    ->required(),
                 Forms\Components\Textarea::make('content')
                     ->required()
                     ->columnSpanFull(),
@@ -45,6 +50,8 @@ class CommentResource extends Resource
                 Tables\Columns\TextColumn::make('post_id')
                     ->numeric()
                     ->sortable(),
+                Tables\Columns\TextColumn::make('content')
+                    ->searchable(),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
@@ -68,6 +75,20 @@ class CommentResource extends Resource
             ]);
     }
 
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Section::make('Solar Panel info')
+
+                    ->schema([
+                        TextEntry::make('user_id'),
+                        TextEntry::make('post_id'),
+                        TextEntry::make('content'),
+                    ])->columns(3)
+            ]);
+    }
+
     public static function getRelations(): array
     {
         return [
@@ -80,7 +101,6 @@ class CommentResource extends Resource
         return [
             'index' => Pages\ListComments::route('/'),
             'create' => Pages\CreateComment::route('/create'),
-            'view' => Pages\ViewComment::route('/{record}'),
             'edit' => Pages\EditComment::route('/{record}/edit'),
         ];
     }
